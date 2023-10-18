c672a0f648811fee12b968474aedea1cf13c4098
twig <willitw@ssfs.org>
2023-10-18 08:57:41 -0400

    even_odd.py done

--- even_odd.py
+++ even_odd.py
@@ -1,19 +1,23 @@
+# even_odd.py
+# Twig Williams
+# 10/18/23
 
 def even_or_odd(number):
-    """
-    Create a function that will tell if a number is even or odd. Use two if statements to do this.
-    :param number: could be any positive or negative integer
-    :return: either "x is an even number" or "x is an odd number"
-    """
-    pass
-
+    if number % 2 == 0:
+        return str(number) + " is an even number"
+    if number % 2 == 1:
+        return str(number) + " is an odd number"
 
 def main():
-    # First, make sure to delete the word "pass" then get input from the user.
-    # They should type in a number, make sure to convert it to an int
-    # Next, call the even_or_odd function, and make sure to pass the user's number as a parameter.
-    pass
+    user_input = input("Please enter a number: ")
+    number = int(user_input)
+    result = even_or_odd(number)
+    print(result)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
+
+
+
+#shabingus
